b7b003ec31c0aee9e97b9165939a7f0d80072d8c
loong917 <loong.me@outlook.com>
2024-01-22 15:20:01 +0800

    loong blog

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+// 1. 从 `astro:content` 导入
+import { z, defineCollection } from 'astro:content';
+// 2. 定义集合
+const blogCollection = defineCollection({
+    type: 'content', // v2.5.0 及之后
+    schema: z.object({
+        id: z.number(),
+        title: z.string(),
+        date: z.string(),
+        tag: z.string(),
+    }),
+});
+// 3. 导出一个 `collections` 对象来注册集合
+export const collections = {
+    'blogs': blogCollection,
+};
